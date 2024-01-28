800fdda6c5b6a65b25df7d255ee539bff71f4e66
workout38 <hikaru.2014.980410@gmail.com>
2024-01-28 19:38:31 +0900

    unicorn.rbを作成

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,34 +1,30 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+# rootパスのディレクトリを指定
+root_path = File.expand_path('../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
-#リクエストを受け付けレスポンスを生成するworker(ワーカー)の数を決める
-worker_processes 1
+# アプリケーションサーバの性能を決定する
+worker_processes 2
 
-#アプリケーションの設置されているディレクトリを指定
-#UnicornがRailsのコードを動かす際、ルーティングなど実際に参照するファイルを探すディレクトリを指定する
-working_directory app_path
+# アプリケーションの設置されているディレクトリを指定
+working_directory root_path
 
-#Unicornの起動に必要なファイルの設置場所を指定
-#Unicornは、起動する際にプロセスidが書かれたファイルを生成する。その場所を指定。
-pid "#{app_path}/tmp/pids/unicorn.pid"
+# プロセスIDの保存先を指定
+pid "#{root_path}/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
-#どのポート番号のリクエストを受け付けることにするかを決定する。今回は、3000番ポートを指定。
-listen 3000
+# ポート番号を指定
+listen "#{root_path}/tmp/sockets/unicorn.sock"
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/log/unicorn.stderr.log"
+# エラーのログを記録するファイルを指定
+stderr_path "#{root_path}/log/unicorn.stderr.log"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# 通常のログを記録するファイルを指定
+stdout_path "#{root_path}/log/unicorn.stdout.log"
 
-#Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
+#応答時間を待つ上限時間を設定
+timeout 30
 
-#以下は応用的な設定なので説明は割愛
-
+# ダウンタイムなしでUnicornを再起動時する
 preload_app true
+
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
